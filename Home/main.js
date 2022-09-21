48e03ff66a044c3dc036b44f87d28264c301e29d
user.name <anselmodiasyahoo@gmail.com>
2022-09-21 13:52:48 -0300

    iniciando a construção da home e o navigation

---
+++ Home/main.js
@@ -0,0 +1,34 @@
+// const nav = document.querySelectorAll('#header nav')
+// const elementToggle = document.querySelectorAll('nav .toggle')
+
+
+// for (const element of elementToggle) {
+//     element.addEventListener('click', function () {
+//         nav.classList.toggle('show')
+//     })
+// }
+
+// // quando clicar no item do menu, esconde o menu
+
+// const links = document.querySelectorAll('nav ul li a')
+
+// for (const link of links) {
+//     link.addEventListener('click', function () {
+//         nav.classList.remove('show')
+//     })
+// }
+
+
+ const open = document.querySelectorAll('nav .open')
+ const close = document.querySelectorAll('nav .close')
+
+ 
+
+
+// function openMenu() {
+//     open.classList.remove('show')
+// }
+  
+  function closeMenu() {
+    close.classList.remove('show')
+}
